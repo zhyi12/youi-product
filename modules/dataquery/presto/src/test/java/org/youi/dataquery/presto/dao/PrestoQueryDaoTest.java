18577b164084001223307380e257ae9eea5935b2
zhyi12 <zhouyi@gwssi.com.cn>
2019-08-23 18:45:57 +0800

    初始化上传

--- modules/dataquery/presto/src/test/java/org/youi/dataquery/presto/dao/PrestoQueryDaoTest.java
+++ modules/dataquery/presto/src/test/java/org/youi/dataquery/presto/dao/PrestoQueryDaoTest.java
@@ -1,5 +1,6 @@
 package org.youi.dataquery.presto.dao;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.youi.dataquery.engine.entity.QueryOrder;
 import org.youi.dataquery.presto.ModuleConfig;
 import org.youi.framework.core.orm.Pager;
+import org.youi.framework.core.orm.PagerRecords;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +34,9 @@ public class PrestoQueryDaoTest {
         queryOrder.setProperty("catalogItemId");
         queryOrders.add(queryOrder);
 
-//        String querySql="select * from stats_agency";
         String querySql = "select sum(S203_2018020170000_D1) S203_2018020170000_D1,DATA_101_COL007,tc.catalogItemId from stats_working_task_row_st001 t left join stats_working_task_row_st001_catalog tc on t._id=tc.rowId group by DATA_101_COL007,tc.catalogItemId";
-        System.out.println(prestoQueryDao.queryRowDataByPager(pager,queryOrders,querySql,new String[]{}).getRecords());
+        PagerRecords pagerRecords = prestoQueryDao.queryRowDataByPager(pager,queryOrders,querySql,new String[]{});
+
+        Assert.assertTrue(pagerRecords.getTotalCount()>0);
     }
 }
