a7bfed7410ebdaca5e8921912c8cbf93f40122fd
zhyi12 <zhouyi@gwssi.com.cn>
2019-09-02 14:17:36 +0800

    初始化上传

---
+++ services/server-metadata/src/main/java/org/youi/product/metadata/MetadataServerStarter.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.youi.product.metadata;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RestController;
+import org.youi.framework.context.ModulesRunnerBuilder;
+import org.youi.framework.services.config.ServiceConfig;
+import org.youi.service.boot.ResourceServerConfig;
+
+/**
+ *
+ */
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+@EnableOAuth2Sso
+public class MetadataServerStarter {
+
+    public static void main(String[] args) {
+        new ModulesRunnerBuilder(ServiceConfig.class,
+                ResourceServerConfig.class,
+                MetadataServerStarter.class).run(args);
+    }
+}
