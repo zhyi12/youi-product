690980aad85f2ed3d7d9fb032111951c52a924f2
zhyi12 <zhouyi@gwssi.com.cn>
2019-09-28 10:13:53 +0800

    删除测试的RequestMapping方法

--- services-common/server-base/src/main/java/org/youi/base/BaseServerStarter.java
+++ services-common/server-base/src/main/java/org/youi/base/BaseServerStarter.java
@@ -15,26 +15,15 @@
  */
 package org.youi.base;
 
-import io.swagger.annotations.ApiOperation;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.config.client.ConfigServicePropertySourceLocator;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.integration.config.EnableIntegration;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.servlet.support.RequestContext;
-import org.springframework.web.servlet.support.RequestContextUtils;
-import org.youi.agency.entity.Agency;
 import org.youi.framework.context.ModulesRunnerBuilder;
 import org.youi.framework.services.config.ServiceConfig;
 import org.youi.service.boot.ResourceServerConfig;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -46,18 +35,6 @@ import java.util.List;
 @EnableOAuth2Sso
 @EnableSwagger2
 public class BaseServerStarter {
-
-    @ApiOperation(value = "获取机构列表",notes = "获取用户机构详细描述")
-    @RequestMapping(value = "/getAgencies.json",method = RequestMethod.GET)
-    public List<Agency> getAgencies(String id){
-        ConfigServicePropertySourceLocator configServicePropertySourceLocator;
-        List list = new ArrayList<>();
-        Agency agency = new Agency();
-        agency.setAreaId("111111");
-        agency.setCode("code");
-        list.add(agency);
-        return list;
-    }
 
     public static void main(String[] args) {
         new ModulesRunnerBuilder(ServiceConfig.class,
