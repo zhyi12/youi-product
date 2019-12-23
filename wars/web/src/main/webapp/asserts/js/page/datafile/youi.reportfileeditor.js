eec14edb4fa32f5709a088cf1533a32422a5464f
zhyi12 <zhouyi@gwssi.com.cn>
2019-12-23 19:16:56 +0800

    交叉表文件编辑器

--- wars/web/src/main/webapp/asserts/js/page/datafile/youi.reportfileeditor.js
+++ wars/web/src/main/webapp/asserts/js/page/datafile/youi.reportfileeditor.js
@@ -26,6 +26,7 @@
             fileUrl:'',//获取数据文件内容
             getModelUrl:'',//获取报表文件模型
             saveModelUrl:'',//保存模型
+            saveDataUrl:'',//保存数据
             bindResize:true,
             useModelTree:true,
         },
@@ -92,13 +93,21 @@
                 this._parseModel({});
             }
         },
-
+        /**
+         * 解析模型数据，生成模型树，同时标识sheet区域显示
+         * @param result
+         * @private
+         */
         _parseModel:function(result){
             this.model = $.extend({},result.record.crossReport);
             this.model.mainAreas  = this.model.mainAreas||[];
             this.model.slaveAreas  = this.model.slaveAreas||[];
 
             var mainAreaNodes = [],slaveAreaNodes = [],headerNodes = [];
+
+            if(this.model.headerItems){
+                headerNodes = _buildItemNodes(this.model.headerItems,'','hi_');
+            }
 
             if(this.model.mainAreas){//主栏
                 for(var i=0;i<this.model.mainAreas.length;i++){
@@ -116,11 +125,23 @@
             }
 
             var treeHtmls = [];
-            treeHtmls.push($.youi.treeUtils.treeNodeHtml('header_node','表头项',{},{group:'header-items',children:headerNodes}));
+            treeHtmls.push($.youi.treeUtils.treeNodeHtml('header_node','表头项',{},{expanded:true,group:'header-items',children:headerNodes}));
             treeHtmls.push($.youi.treeUtils.treeNodeHtml(_MAIN_CELL_CLASS+'_node','主栏',{},{expanded:true,children:mainAreaNodes}));
             treeHtmls.push($.youi.treeUtils.treeNodeHtml(_SLAVE_CELL_CLASS+'_node','宾栏',{},{expanded:true,children:slaveAreaNodes}));
 
             this.modelTreeElem.find('>ul').html(treeHtmls.join(''));
+
+            //
+            this._activeDataSave();
+        },
+
+        _activeDataSave:function(){
+            var groups = [];
+            if(this.model.mainAreas && this.model.mainAreas.length
+                && this.model.slaveAreas && this.model.slaveAreas.length){
+                groups.push('data');
+            }
+            this.element.trigger('activeTools',{groups:groups});
         },
 
         _renderBlockArea:function(blockArea,type){
@@ -202,10 +223,11 @@
             this._setMetaArea(_DIMENSION_YEAR,'年份');
         },
 
-
+        /**
+         * 保存交叉表式
+         */
         save:function(dom,commandOptions){
             var model = this._getModel();
-
             if(this.options.saveModelUrl){
                 $.youi.ajaxUtils.ajax({
                     url:this.options.saveModelUrl,
@@ -219,6 +241,23 @@
         },
 
         /**
+         * 保存数据
+         */
+        saveData:function(){
+            var model = this._getModel(),
+                datas = this._getDatas(model);//二维数组数据
+            if(this.options.saveDataUrl) {
+                $.youi.ajaxUtils.ajax({
+                    url: this.options.saveDataUrl,
+                    data: $.youi.recordUtils.recordToAjaxParamStr($.extend({}, model, {texts: datas})),
+                    success: function (result) {
+
+                    }
+                });
+            }
+        },
+
+        /**
          *
          * @param dom
          * @param commandOptions
@@ -226,7 +265,7 @@
         showCrossTable:function(dom,commandOptions){
             var model = this._getModel(),
                 cubes = _modelToCubes(model);
-            this._openSubPage('crossTable',{record:{cubes:cubes}},{});
+            this._openSubPage('crossTable',{record:{model:model,cubes:cubes}},{});
         },
 
         /**************************************  树右键菜单调用方法 **************************************/
@@ -276,10 +315,101 @@
         },
 
         /**************************************** 弹出窗口回调方法 ***************************************/
+        /**
+         * 构建打开新增年份框填充的数据对象
+         */
+        _addPubYearFormRecord:function (record) {
+            return this._addPubItemFormRecord(record);
+        },
+        /**
+         * 新增公共年份成功后
+         */
+        _addPubYearAfterFormSubmit:function(record){
+            var year = record.id.substr(0,4);
+            this._addPubItemAfterFormSubmit($.extend({},record,{id:year,mappedId:year,text:year+'年',type:_DIMENSION_YEAR}));
+        },
+
+        _addPubIndicatorFormRecord:function (record) {
+            return this._addPubItemFormRecord(record);
+        },
+        /**
+         * 新增公共指标成功后
+         */
+        _addPubIndicatorAfterFormSubmit:function(record){
+            this._addPubItemAfterFormSubmit($.extend({mappedId:record.id},record,{type:_DIMENSION_INDICATOR}));
+        },
+        /**
+         * 窗口填充：新增公共项
+         */
+        _addPubItemFormRecord:function (record) {
+            return {'parentId':record.id};
+        },
+
+        /**
+         * 窗口提交：关联月份
+         */
+        _addPubItemAfterFormSubmit:function (record) {
+            if(record.parentId){
+                var parentNode = this._findTreeNodeById(record.parentId);
+                this._addTreeNode(parentNode,'',record.text,{id:record.id,'mapped-id':record.mappedId||'','dim-id':record.type+'s'},{tooltips:record.mappedId||'',group:record.type+'-item'},record.parentId+record.id);
+            }
+        },
+
+        /**
+         * 关联月份
+         */
+        _refMonthAfterFormSubmit:function (record) {
+            this._refItemAfterFormSubmit(record);
+        },
+        /**
+         * 关联属性
+         */
+        _refAttrAfterFormSubmit:function (record) {
+            this._refItemAfterFormSubmit(record);
+        },
+        /**
+         * 关联报告期
+         */
+        _refPeriodAfterFormSubmit:function (record) {
+            this._refItemAfterFormSubmit(record);
+        },
+
+        /**
+         * 关联报告期维度
+         */
+        _refPeriodsAfterFormSubmit:function (record) {
+            this._refItemsAfterFormSubmit(record);
+        },
+
+        _refItemsAfterFormSubmit:function (record) {
+            var dimTreeNode = this._findTreeNodeById(record.id);
+            for(var i=0;i<record.items.length;i++){
+                var item = record.items[i];
+                var itemNode = dimTreeNode.find('.treeNode[data-item-id='+item.id+']');
+                if(item.mappedId){
+                    itemNode.attr('data-mapped-id',item.mappedId).data('mappedId',item.mappedId);
+                    itemNode.attr('data-item-id',item.mappedId).data('item',item.mappedId);
+                    itemNode.attr('data-level',item.level).data('level',item.level);
+                    this._setTreeNodeToolTips(itemNode,'['+item.mappedId+']');
+                }
+            }
+        },
+        /**
+         * 窗口填充：关联维度报告期
+         */
+        _refPeriodsFormRecord:function (record) {
+            return $.extend({items:this._parseItemsFromModelTree(record.id)},record);
+        },
+
         //
-        // _renameFormRecord:function (record) {
-        //     return record;
-        // },
+        _refItemAfterFormSubmit:function (record) {
+            var itemNode = this._findTreeNodeById(record.id);
+            //
+            this._setTreeNodeToolTips(itemNode,'['+record.mappedId+']');
+            itemNode.data('mappedId',record.mappedId).attr('data-mapped-id',record.mappedId)
+                    .data('itemId',record.mappedId).attr('data-item-id',record.mappedId);
+        },
+
 
         _renameAfterFormSubmit:function(record){
             var treeNode = this._findTreeNodeById(record.id);
@@ -320,6 +450,10 @@
          * @private
          */
         _setBlockArea:function(clazz,position,caption) {
+            if(this._hasBlockArea()){
+                $.youi.messageUtils.showMessage('该区域存在已经设置的单元格');
+                return;
+            }
             var blockAreaId = '', dataAttrName = "data-"+clazz,
                 blockArea = this._processArea(function (cellRecord,cellElem,rowIndex,colIndex,curArea) {
                     if(rowIndex==curArea.startRow+1 && colIndex==curArea.startCol){
@@ -333,6 +467,10 @@
             var children = [{id:blockAreaId+'Pub',text:'公共项',group:'pub-items'}];
             this.modelTreeElem.tree('addNode',this.modelTreeElem.find('.treeNode#'+clazz+'_node'),blockAreaId,caption+'区域'+blockAreaId,
                 _buildAreaDataProp(blockArea),{group:clazz,expanded:true,children: children});
+        },
+
+        _hasBlockArea:function(){
+            return this.element.find('.cell.selected.'+_MAIN_CELL_CLASS+',.cell.selected.'+_SLAVE_CELL_CLASS).length>0;
         },
 
         _setMetaArea:function(type,caption){
@@ -351,7 +489,7 @@
             blockArea.dimensions.push($.extend({id:dimensionId,items:items,group:type},area));
 
             $(items).each(function () {
-                children.push($.extend({group:type+'-item',datas:{'dim-id':type}},this));
+                children.push($.extend({group:type+'s-item',datas:{'dim-id':type}},this));
             });
             //
             var treeData = {children:children,group:type+'s',expanded:true};
@@ -369,7 +507,33 @@
             this._refreshDimensionItems(_MAIN_CELL_CLASS);
             //宾栏
             this._refreshDimensionItems(_SLAVE_CELL_CLASS);
+            //headerItems
+            this.model.headerItems = this._parseItemsFromModelTree('header_node');
             return this.model;
+        },
+
+        /**
+         * 基于模型和sheet获取立方体数据
+         * @param model
+         * @private
+         */
+        _getDatas:function(model){
+            //
+            var datas = [],
+                dataArea = {
+                startRow:model.mainAreas[0].startRow,
+                endRow:model.mainAreas[model.mainAreas.length-1].endRow,
+                startCol:model.slaveAreas[0].startCol,
+                endCol:model.slaveAreas[model.slaveAreas.length-1].endCol,
+            };
+            dataArea.rows = dataArea.endRow - dataArea.startRow +1;
+            dataArea.cols = dataArea.endCol - dataArea.startCol +1;
+
+            this.contentElem.sheet('processArea',dataArea,function (cellRecord,cellElem,rowIndex,colIndex,curArea){
+                datas[rowIndex - curArea.startRow-1] = datas[rowIndex - curArea.startRow-1]||{texts:[]};
+                datas[rowIndex - curArea.startRow-1].texts.push($.trim(cellRecord.text)||'-');
+            });
+            return datas;
         },
         /**
          *
@@ -382,26 +546,54 @@
             for(var i=0;i<areas.length;i++){
                 var area = areas[i];
                 area.text = this._findNodeText(area.id);
+                area.pubItems = this._parseItemsFromModelTree(area.id+'Pub');
+
                 if(area.dimensions){
+                    var deletedDimensionIds = [];
                     for(var j=0;j<area.dimensions.length;j++){
                         var dimension = area.dimensions[j];
                         dimension.text = this._findNodeText(dimension.id);
                         dimension.items = this._parseItemsFromModelTree(dimension.id);
+                        //
+                        var dimensionNode = this._findTreeNodeById(dimension.id);
+
+                        if(!dimensionNode||dimensionNode.length==0){
+                            deletedDimensionIds.push(dimension.id);
+                        }
                     }
+                    var areaDimensions = [];
+                    $(area.dimensions).each(function(){
+                        if($.inArray(this.id,deletedDimensionIds)==-1){
+                            areaDimensions.push(this);
+                        }
+                    });
+                    //
+                    area.dimensions = areaDimensions;
                 }
             }
         },
 
+        /**
+         * 根据模型树的节点ID查找树节点文本
+         * @param treeNodeId
+         * @private
+         */
         _findNodeText:function(treeNodeId){
             return $.youi.treeUtils.getNodeText(this._findTreeNodeById(treeNodeId));
         },
 
+        /**
+         * 在树的子节点中解析items
+         * @param containerId
+         * @returns {Array}
+         * @private
+         */
         _parseItemsFromModelTree:function(containerId){
             var items = [];
             this.modelTreeElem.find('.treeNode#'+containerId).find('.treeNode').each(function () {
                 var elem = $(this);
                 items.push($.extend({
-                    id:elem.attr('id'),
+                    id:elem.data('itemId')||elem.attr('id'),
                     text:$.youi.treeUtils.getNodeText(elem)
                 },elem.data()));
             });
@@ -534,13 +726,17 @@
      * @private
      */
     function _buildAreaNode(blockArea,type) {
-        return {id:blockArea.id,text:blockArea.text||blockArea.id,group:type,datas:{
+        //pubItems
+        blockArea.pubItems = blockArea.pubItems||[];
+
+        return {id:blockArea.id,text:blockArea.text||blockArea.id,group:type,expanded:true,datas:{
                     'start-row':blockArea.startRow,
                     'start-col':blockArea.startCol,
                     'end-row':blockArea.endRow,
                     'end-col':blockArea.endCol,
-                },children:_buildDimensionNodes(blockArea.dimensions)};
+                },children:[{'id':blockArea.id+'Pub',expanded:true,text:'公共项',group:'pub-items',children:_buildItemNodes(blockArea.pubItems,'',blockArea.id+'Pub')}].concat(_buildDimensionNodes(blockArea.dimensions))};
     }
+
     /**
      *
      * @param dimensions
@@ -552,25 +748,29 @@
             nodes.push({
                 id:this.id,
                 text:this.text||this.id,
-                group:this.items[0].dimId+'s',
+                group:this.group+'s',
                 datas:{
                     'start-row':this.startRow,
                     'start-col':this.startCol,
                     'end-row':this.endRow,
                     'end-col':this.endCol,
                 },
-                children:_buildItemNodes(this.items)
+                children:_buildItemNodes(this.items,this.group,this.id)
             });
         });
         return nodes;
     }
 
-    function _buildItemNodes(items) {
+    function _buildItemNodes(items,group,prefix) {
         var nodes = [];
+        prefix = prefix||'';
         $(items).each(function () {
             nodes.push({
-                id:this.id,
-                group:this.dimId+'-item',
+                id:prefix+this.id,
+                dimId:this.dimId||'',
+                tooltips:this.mappedId||'',
+                datas:{'item-id':this.id,'mapped-id':this.mappedId,'dim-id':this.dimId||'',level:this.level},
+                group:(this.dimId||group)+'s-item',
                 text:this.text||this.id
             });
         });
@@ -578,7 +778,7 @@
     }
 
     /**
-     * 模型转立方体集合
+     * 模型转立方体集合，只输出一个cube，主宾栏多项合并
      * @private
      */
     function _modelToCubes(model) {
@@ -588,12 +788,18 @@
 
     }
 
+    /**
+     * 合并区域集合的维度项为一个维度（主、宾）
+     * @param areas
+     * @param type
+     * @returns {Array}
+     * @private
+     */
     function _buildCubeItems(areas,type) {
         var crossItems = [],items = [];
         $(areas).each(function () {
             crossItems = crossItems.concat($.youi.crossTableUtils.expandCrossItems(this.dimensions));
         });
-
         //
         $(crossItems).each(function (index) {
             var texts = [];
@@ -605,7 +811,6 @@
                 text:texts.join('/')
             });
         });
-
         return items;
     }
 })(jQuery);
