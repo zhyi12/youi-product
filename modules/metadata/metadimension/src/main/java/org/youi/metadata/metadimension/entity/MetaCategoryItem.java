ea04401697f01a1f1ad33bd42839ec8fa1cdd810
zhyi12 <zhouyi@gwssi.com.cn>
2019-12-23 21:02:16 +0800

    维度模块

---
+++ modules/metadata/metadimension/src/main/java/org/youi/metadata/metadimension/entity/MetaCategoryItem.java
@@ -0,0 +1,106 @@
+/*
+ * YOUI框架
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.youi.metadata.metadimension.entity;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.youi.framework.core.dataobj.Domain;
+
+import javax.persistence.Column;
+/**
+ * 实体: 分类项
+ * @author 代码生成器
+ * @since 1.0.0
+ */
+@Document(collection = "youi_metadata_category_item")
+public class MetaCategoryItem implements Domain{
+	
+	private static final long serialVersionUID = 7188855707826907784L;
+	
+	@Id
+	@Column
+	private String id;//主键
+
+	@Column
+	private String text;//名称
+
+	@Column
+	private String expression;//表达式
+
+	public String getId(){
+		return this.id;
+	}
+	
+	public void setId(String id){
+		this.id = id;
+	}
+	public String getText(){
+		return this.text;
+	}
+	
+	public void setText(String text){
+		this.text = text;
+	}
+	public String getExpression(){
+		return this.expression;
+	}
+	
+	public void setExpression(String expression){
+		this.expression = expression;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((text == null) ? 0 : text.hashCode());
+		result = prime * result + ((expression == null) ? 0 : expression.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		final MetaCategoryItem other = (MetaCategoryItem) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (text == null) {
+			if (other.text != null)
+				return false;
+		} else if (!text.equals(other.text))
+			return false;
+		if (expression == null) {
+			if (other.expression != null)
+				return false;
+		} else if (!expression.equals(other.expression))
+			return false;
+		return true;
+	}
+	
+	public String toString(){
+		return super.toString();
+	}
+}
---
+++ modules/metadata/metadimension/src/main/java/org/youi/metadata/metadimension/service/MetaAttrManager.java
@@ -0,0 +1,74 @@
+/*
+ * YOUI框架
+ * Copyright 2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.youi.metadata.metadimension.service;
+
+import java.util.List;
+import java.util.Collection;
+
+import org.youi.framework.core.orm.Condition;
+import org.youi.framework.core.orm.Order;
+import org.youi.framework.core.orm.Pager;
+import org.youi.framework.core.orm.PagerRecords;
+import org.youi.framework.esb.annotation.ConditionCollection;
+import org.youi.framework.esb.annotation.EsbServiceMapping;
+import org.youi.framework.esb.annotation.OrderCollection;
+import org.youi.framework.esb.annotation.ServiceParam;
+
+import org.youi.metadata.metadimension.entity.MetaAttr;
+
+/**
+ * <p>系统描述: </p>
+ * <p>功能描述 :</p>
+ * @author 代码生成器
+ * @since 1.0.0
+ */
+public interface MetaAttrManager{
+
+    /**
+     * 条件查询列表
+     */
+    @EsbServiceMapping(trancode="8001090101",caption="主键查询时间属性")
+    List<MetaAttr> getMetaAttrs(
+        @ConditionCollection(domainClazz=MetaAttr.class) Collection<Condition> conditions,
+        @OrderCollection Collection<Order> orders);
+    /**
+     * 根据主键查询
+     */
+    @EsbServiceMapping(trancode="8001090102",caption="主键查询时间属性")
+    MetaAttr getMetaAttr(@ServiceParam(name="id") String id);
+
+	/**
+	 * 分页查询用户
+	 * @return 分页对象
+	 */
+    @EsbServiceMapping(trancode="8001090103",caption="分页查询时间属性")
+    PagerRecords getPagerMetaAttrs(Pager pager,//分页条件
+    @ConditionCollection(domainClazz=MetaAttr.class) Collection<Condition> conditions,//查询条件
+        @OrderCollection Collection<Order> orders);
+    /**
+     * 保存并返回对象
+     */
+    @EsbServiceMapping(trancode="8001090104",caption="保存时间属性")
+    MetaAttr saveMetaAttr(MetaAttr o);
+
+    /**
+     * 根据主键集合删除对象
+     * @param id
+     */
+    @EsbServiceMapping(trancode="8001090105",caption="主键删除时间属性")
+    void removeMetaAttr(@ServiceParam(name="id") String id);
+}
