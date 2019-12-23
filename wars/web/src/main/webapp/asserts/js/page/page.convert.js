cc7384cc0c507451b70fa7a0134b73d2b8f82e98
zhyi12 <zhouyi@gwssi.com.cn>
2019-12-23 19:40:47 +0800

    使用javascript文件存储固定的convert，主要用于开发调试。

---
+++ wars/web/src/main/webapp/asserts/js/page/page.convert.js
@@ -0,0 +1,10 @@
+
+(function($) {
+    var _log = $.youi.log;
+
+
+    $.extend($.youi.serverConfig.convertArray,{
+        'periodType':{"1":"年度","2":"半年","3":"季度","4":"月度"}
+    });
+
+})(jQuery);
